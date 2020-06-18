fed1932543d2d3329e34ecb2f08001886737e58d
exesse <kulbatski@gmail.com>
2020-06-18 17:20:43 +0200

    fast sorting

--- algorithms.py
+++ algorithms.py
@@ -4,7 +4,7 @@
 def binary_search(sorted_list, value) -> int:
     """
     Function search for the position of given value in sorted list.
-    Returns index if found.
+    Returns index if found. Time is: O(log(n)).
     :param sorted_list: list
     :param value: int
     :return: int
@@ -53,3 +53,54 @@ def rec_biggest(numbers, x=0):
         x = numbers[0]
     numbers.pop(0)
     return rec_biggest(numbers, x)
+
+
+class SelectionSort:
+    """
+    Function performs sorting by selection.
+    Average time is 0(n*n).
+    """
+    def __init__(self, array):
+        self.array = array
+
+    @staticmethod
+    def smallest(array) -> int:
+        """
+        Function finds smallest value in given array.
+        :param array: list, unsorted int list
+        :return: int, index for lowest int in given list
+        """
+        low = array[0]
+        low_index = 0
+        for i in range(1, len(array)):
+            if array[i] < low:
+                low = array[i]
+                low_index = i
+        return low_index
+
+    def sort(self) -> list:
+        """
+        Function generates new sorted list.
+        :return result_array: list, new sorted list
+        """
+        result_array = []
+        for i in range(len(self.array)):
+            low = self.smallest(self.array)
+            result_array.append(self.array.pop(low))
+        return result_array
+
+
+def quick_sort(array) -> list:
+    """
+    Function performs quick sort for average time:
+                0(n*log(n))
+    :param array: list, list of non sorted values
+    :return: list, sorted list
+    """
+    if len(array) < 2:
+        return array
+    pivot = array[0]
+    left_shoulder = [i for i in array if i < pivot]
+    right_shoulder = [i for i in array if i > pivot]
+    return quick_sort(left_shoulder) + [pivot] + quick_sort(right_shoulder)
+
