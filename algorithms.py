b5d76d97d0848fcd8de48cbfc4ce9067c57c56b5
exesse <kulbatski@gmail.com>
2020-06-18 16:17:56 +0200

    recursion

--- algorithms.py
+++ algorithms.py
@@ -24,3 +24,32 @@ def binary_search(sorted_list, value) -> int:
 
         elif sorted_list[mid] == value:
             return mid
+
+
+def rec_sum(numbers, x=0) -> int:
+    """
+    Function return the sum of given list using recursion.
+    :param numbers: list, list of integers
+    :param x: int, current sum
+    :return: int, total sum
+    """
+    if len(numbers) == 0:
+        return x
+    x += numbers[0]
+    numbers.pop(0)
+    return rec_sum(numbers, x)
+
+
+def rec_biggest(numbers, x=0):
+    """
+    Function uses recusrion to find biggest number in given list.
+    :param numbers: list, list of integers
+    :param x: int, current highest
+    :return: int, the highest given
+    """
+    if len(numbers) == 0:
+        return x
+    elif numbers[0] >= x:
+        x = numbers[0]
+    numbers.pop(0)
+    return rec_biggest(numbers, x)
